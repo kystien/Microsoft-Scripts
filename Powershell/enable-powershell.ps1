fc2944299a505027bd49fb425d1d6c2f5a7756cd
Jesse Poland <jpoland25@gmail.com>
2018-10-23 22:05:01 -0600

    Delete enable-powershell.ps1

--- Powershell/enable-powershell.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy remotesigned -Force
-Enable-PSRemoting -Force
-winrm set winrm/config/client '@{TrustedHosts="*"}'
-winrm set winrm/config/client '@{AllowUnencryoted="true"}'
-winrm set winrm/config/service '@{AllowUnencryoted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{EnableCompatibilityHttpListener="true"}'
-winrm set winrm/config/service '@{EnableCompatibilityHttpsListener="true"}'
-Exit-PSSession
