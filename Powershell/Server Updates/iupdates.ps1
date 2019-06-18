528bf869562acc985cb30b28cea6f1eded426eb2
Jesse Poland <20099828+kystien@users.noreply.github.com>
2019-06-18 14:19:59 -0600

    Create iupdates.ps1

---
+++ Powershell/Server Updates/iupdates.ps1
@@ -0,0 +1,7 @@
+$dir = (Get-Item -Path ".\" -Verbose).FullName
+ Foreach($item in (ls $dir *.msu -Name))
+ {
+    echo $item
+    $item = $dir + "\" + $item
+    wusa $item /quiet /norestart | Out-Null
+ }
